335aa8834d11a270070b51f8ce8c0236106607b1
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2024-01-06 13:33:24 +0530

    Add files via upload

---
+++ P02-Operations.py
@@ -0,0 +1,15 @@
+# P02-operations.py
+# Multiplication: 550 * 10
+print(550 * 10)
+
+# Subtraction: 550 - 654
+print(550 - 654)
+
+# Addition: 65 + 54
+print(65 + 54)
+
+# Division: 550 / 10
+print(550 / 10)
+
+# Modulus: 550 % 10
+print(550 % 10)
