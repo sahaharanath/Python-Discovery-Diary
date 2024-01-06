61a2be694445c96a4e61f1ab77e3d764417f4c66
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2024-01-06 13:31:52 +0530

    Add files via upload

---
+++ P01-Hello_World.py
@@ -0,0 +1,10 @@
+# P01-Hello_World.py
+# This simple Python script prints "Hello World!" to the console.
+
+print("Hello World!")
+
+# Learning a new way
+#1st way :
+print("Hello World! 12345")
+#2nd way :
+print("We can do this also! ", 12345)
