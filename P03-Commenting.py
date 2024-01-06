0899f79d55683dd5f868e78e2c3bebce5c5875a4
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2024-01-06 13:33:47 +0530

    Add files via upload

---
+++ P03-Commenting.py
@@ -0,0 +1,21 @@
+# P03-commenting.py
+print("Here is how to comment a line in Python!")
+# And here I am commenting this line!
+
+print("Here the 1st way to comment multiple lines:")
+# 1st line
+# 2nd line
+# 3rd line
+# 4th line
+# 5th line
+
+print("Here the 2nd way to comment multiple lines:")
+print("We have to use triple single quotes: '''")
+
+'''
+1st line
+2nd line
+3rd line
+4th line
+5th line
+'''
