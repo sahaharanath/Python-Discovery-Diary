522e6adf5bb457af03f735f885e3a59131ca7f57
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2024-01-06 13:34:00 +0530

    Add files via upload

---
+++ P04-Escape Characters.py
@@ -0,0 +1,10 @@
+# P04-escape_characters.py
+# We use \n to separate lines, and \n is considered as one character, not 2 characters
+print("Here is the first line of code \nNow here is the second line of code!")
+
+# To handle quotes within a string, use \"
+print("She said, \"That she is a good girl.\" And I knew it before!")
+# Here also \" is treated as a single character!
+
+# For single quote (') use \'
+print('This is \'PyCharm\' And It is amazing')
