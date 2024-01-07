97547a9b78e1252bd06d28351b548e665fb28cef
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2024-01-07 19:45:07 +0530

    Demonstrates arithmetic operations on variables a and b with comments for clarity.

---
+++ P08-Arithmetic Operations.py
@@ -0,0 +1,32 @@
+# P08-Arithmetic Operations.py
+# Demonstrates arithmetic operations on variables a and b with comments for clarity.
+a = 5215
+b = 25
+
+# Addition
+print("The value of", a, "+", b, "is equal to:", a + b)
+print(a, "+", b, "=", a + b)
+
+# Subtraction
+print("The value of", a, "-", b, "is equal to:", a - b)
+print(a, "-", b, "=", a - b)
+
+# Multiplication
+print("The value of", a, "*", b, "is equal to:", a * b)
+print(a, "*", b, "=", a * b)
+
+# Division
+print("The value of", a, "/", b, "is equal to:", a / b)
+print(a, "/", b, "=", a / b)
+
+# Modulus (Remainder)
+print("The value of", a, "%", b, "is equal to:", a % b)
+print(a, "%", b, "=", a % b)
+
+# Floor Division
+print("The value of", a, "//", b, "is equal to:", a // b)
+print(a, "//", b, "=", a // b)
+
+# Exponentiation
+print("The value of", a, "**", b, "is equal to:", a ** b)
+print(a, "**", b, "=", a ** b)
