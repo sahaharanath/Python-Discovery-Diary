e1975fc9162ccd7d4f3928f36c874ac1adfefe81
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2024-01-20 21:12:12 +0530

    Demonstrates membership operators in Python.

---
+++ P16-Membership Operators.py
@@ -0,0 +1,12 @@
+# P16-Membership Operators.py
+# Demonstrates membership operators in Python.
+
+# Example 1: 'in' Operator
+print("'in' Operator")
+str1 = "Hello Welcome to Python Code With me"
+print('h' in str1)  # True, as 'h' is present in the string
+
+# Example 2: 'not in' Operator
+print("'not in' Operator")
+str2 = "Hello Welcome to Kolkata"
+print('t' not in str2)  # False, as 't' is present in the string
