7f0be9aab302645c97c9ca5711864fa0f6cdc38c
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2024-01-20 21:02:31 +0530

    Demonstrates comparison operators in Python.

---
+++ P14-Comparison Operators.py
@@ -0,0 +1,38 @@
+# P14-Comparison Operators.py
+# Demonstrates comparison operators in Python.
+
+# Example 1: '==' Operator (Equal)
+print("'==' Operator")
+x = 10
+y = 10
+print(x == y)  # True, as x is equal to y
+
+# Example 2: '!=' Operator (Not Equal)
+print("'!=' Operator")
+a = 5
+b = 6
+print(a != b)  # True, as a is not equal to b
+
+# Example 3: '>' Operator (Greater Than)
+print("'>' Operator")
+c = 15
+d = 8
+print(c > d)  # True, as c is greater than d
+
+# Example 4: '<' Operator (Less Than)
+print("'<' Operator")
+j = 9
+l = 10
+print(j < l)  # True, as j is less than l
+
+# Example 5: '<=' Operator (Less Than or Equal To)
+print("'<=' Operator")
+s = 100
+f = 56
+print(s <= y)  # False, as s is not less than or equal to y
+
+# Example 6: '>=' Operator (Greater Than or Equal To)
+print("'>=' Operator")
+z = 89
+r = 67
+print(z >= r)  # True, as z is greater than or equal to r
