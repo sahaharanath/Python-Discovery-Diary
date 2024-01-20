9bd7920a8b4c0ad2c49ed55b984e493e7f328242
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2024-01-20 20:56:09 +0530

    Demonstrates various assignment operators in Python.

---
+++ P13-Assignment Operators.py
@@ -0,0 +1,19 @@
+# P13-Assignment Operators.py
+# Demonstrates various assignment operators in Python.
+
+# Example 1: '=' Operator
+print("'=' Operator")
+x = 5
+print(x)
+
+# Example 2: '+=' Operator
+print("'+=' Operator")
+a = 5
+a += 4  # Equivalent to a = a + 4
+print(a)
+
+# Example 3: '-=' Operator
+print("-=' Operator")
+b = 6
+b -= 2  # Equivalent to b = b - 2
+print(b)
