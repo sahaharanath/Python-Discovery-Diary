6818a6d7462f9e049f1af6dc89a6b9efeb1235f0
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2024-01-20 21:07:36 +0530

    Demonstrates logical operators in Python.

---
+++ P15-Logical Operators.py
@@ -0,0 +1,19 @@
+# P15-logical_operators.py
+# Demonstrates logical operators in Python.
+
+# Example 1: 'and' Operator
+print("'and' Operator")
+a = 100
+b = 100
+print(a > 25 and b > 55)  # True, as both conditions are true
+
+# Example 2: 'or' Operator
+print("'or' Operator")
+x = 25
+y = 45
+print(x < 10 or y > 250)  # False, as neither condition is true
+
+# Example 3: 'not' Operator
+print("'not' Operator")
+x1 = 15
+print(not x1 != 10)  # False, as the condition x1 != 10 is true, and 'not' negates it
