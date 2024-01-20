3a82f733a363ae386f288547ed0269d9172cefbe
Haranath Saha <144214909+sahaharanath@users.noreply.github.com>
2024-01-20 21:15:51 +0530

    Demonstrates identity operators in Python.

---
+++ P17-Identity Operators.py
@@ -0,0 +1,14 @@
+# P17-identity_operators.py
+# Demonstrates identity operators in Python.
+
+# Example 1: 'is' Operator
+print("'is' Operator")
+x = "What is your favorite color?"
+y = "What is your favorite food?"
+print(x is y)  # False, as x and y have different values
+
+# Example 2: 'is not' Operator
+print("'is not' Operator")
+x1 = "50"
+y1 = "20"
+print(x1 is not y1)  # True, as x1 and y1 have different values
